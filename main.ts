e156e9d26e443abac7b383e60db17e9f2d199fd2
Muhammad  Ali Sheikh <154984806+muhammadali729@users.noreply.github.com>
2024-04-14 04:17:33 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,59 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+let mybalance= 10000;
+let mypin=4729;
+const pin =await inquirer.prompt([
+    {
+        name: "pinnumber",
+        type: "number",
+        message: "Enter your pin number:",
+    },
+]);
+if(pin.pinnumber===mypin){
+    console.log("You can proceed now");
+
+    const operation =await inquirer.prompt(
+        [
+        {
+            name: "want",
+            type: "list",
+            message: "What you want to do:",
+            choices: ["bank balance", "Withdraw"],
+        }
+    ]);
+    if(operation.want=== "bank balance"){
+        console.log(mybalance);
+    }
+    else if(operation.want === "Withdraw"){
+
+        const withdrawamount =await inquirer.prompt(
+            [
+            {
+                name: "amount",
+                type: "number",
+                message: "Enter your withdraw amount",
+
+            }
+
+        ]);
+        if(withdrawamount.amount<=mybalance){
+
+       
+        mybalance= mybalance-withdrawamount.amount;
+        console.log("Your Remaining balance is:" , mybalance);
+
+    }
+    else{
+        console.log("You don't have sufficient balance");
+    }
+    }
+    else{
+        console.log("You choose wrong option");
+    }
+    }
+else{
+    console.log("You Enter wrong pin");
+}
